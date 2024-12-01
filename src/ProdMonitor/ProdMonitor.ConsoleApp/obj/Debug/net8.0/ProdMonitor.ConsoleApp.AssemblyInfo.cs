96af4476172bc879ff0532b782848c51668bc5fc
m4kson <limos.00@mail.ru>
2024-12-01 13:37:37 +0300

    ci added

--- src/ProdMonitor/ProdMonitor.ConsoleApp/obj/Debug/net8.0/ProdMonitor.ConsoleApp.AssemblyInfo.cs
+++ src/ProdMonitor/ProdMonitor.ConsoleApp/obj/Debug/net8.0/ProdMonitor.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProdMonitor.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb4ce6cb5867c7ecdd43b573e4f49d34fd4d586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54efc445320d7975ce4dfdffd9cea2bc6ad643e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProdMonitor.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProdMonitor.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
