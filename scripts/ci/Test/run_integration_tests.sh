dacb597bf8c64a3236bacbfaf8a1b0af9c6a6f42
m4kson <limos.00@mail.ru>
2025-02-15 11:15:11 +0300

    wi[

--- scripts/ci/Test/run_integration_tests.sh
+++ scripts/ci/Test/run_integration_tests.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "Starting integration tests..."
+# echo "Starting integration tests..."
 
-# 1. Убедитесь, что база данных доступна и готова к работе
-echo "Waiting for PostgreSQL to be available..."
+# # 1. Убедитесь, что база данных доступна и готова к работе
+# echo "Waiting for PostgreSQL to be available..."
 
-# Ожидание готовности контейнера PostgreSQL
-until docker exec -it prodmonitor-postgres-container pg_isready -U testuser; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 5
-done
+# # Ожидание готовности контейнера PostgreSQL
+# until docker exec -it prodmonitor-postgres-container pg_isready -U testuser; do
+#   echo "Waiting for PostgreSQL to be ready..."
+#   sleep 5
+# done
 
-echo "PostgreSQL is ready."
+# echo "PostgreSQL is ready."
 
-# 2. Выполнение миграций базы данных (если нужно)
-echo "Running database migrations..."
-dotnet ef database update --project src/ProdMonitor/ProdMonitor.Infrastructure/ProdMonitor.Infrastructure.csproj --no-build --verbose
+# # 2. Выполнение миграций базы данных (если нужно)
+# echo "Running database migrations..."
+# dotnet ef database update --project src/ProdMonitor/ProdMonitor.Infrastructure/ProdMonitor.Infrastructure.csproj --no-build --verbose
 
 # 3. Запуск интеграционных тестов
 echo "Running integration tests..."
